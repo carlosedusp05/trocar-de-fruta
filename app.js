e590950414b078004be157f63f4e7dae1c7dbdbc
CARLOS EDUARDO SILVA PEREIRA <25132872@dsn127m1249386.cfp127aluno.local>
2025-08-04 17:01:00 -0300

    Projeto incompleto, faltando apenas configurar os botões

---
+++ app.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const botaoTrocarFruta = document.getElementById('trocar-fruta')
+
+const fruta = document.getElementById('fruta').value 
+
+function trocarFruta () {
+    document.documentElement.style.setProperty('--bg-imagens', )
+}
