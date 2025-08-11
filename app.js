b9e8a9e6c6d4203ca3ec4bba3a71656badb1584c
CARLOS EDUARDO SILVA PEREIRA <25132872@dsn127m1249386.cfp127aluno.local>
2025-08-11 16:12:57 -0300

    Projeto trocar-fruta feito

--- app.js
+++ app.js
@@ -1,9 +1,52 @@
 'use strict'
-
 const botaoTrocarFruta = document.getElementById('trocar-fruta')
+const inputFruta = document.getElementById('fruta')
+const botaoAutomatico = document.getElementById('automatico')
 
-const fruta = document.getElementById('fruta').value 
-
-function trocarFruta () {
-    document.documentElement.style.setProperty('--bg-imagens', )
+const conjuntoImagens = {
+        "banana": './img/banana.jpeg',
+        "apple": './img/maçã.jpg',
+        "strawberry": './img/morango.jpg',
+        "grape": './img/uva.jpg',
+        "pineapple": './img/abacaxi.jpeg',
 }
+
+let intervalo = null
+
+botaoTrocarFruta.addEventListener('click', trocarFruta);
+
+function trocarFruta() {
+    let  frutaPesquisa = document.getElementById('fruta').value.toLowerCase();
+
+    let imagens = conjuntoImagens
+
+    if (imagens[frutaPesquisa]) {
+        frutaCampo.style.backgroundImage = `url('${imagens[frutaPesquisa]}')`;
+    } else {
+        alert('Fruta não encontrada! Suas opções são: banana, grape, pineapple, strawberry e apple')
+    }
+}
+
+function automatico() {
+    if (intervalo) {
+        clearInterval(intervalo)
+        intervalo = null
+        botaoAutomatico.textContent = "Automatico"
+    } else {
+        let frutas = Object.keys(conjuntoImagens)
+        let indice = 0 
+
+        intervalo = setInterval (() => {
+            document.documentElement.style.setProperty(
+                "--bg-imagens",
+                `url('${conjuntoImagens[frutas[indice]]}')`
+            )
+            inputFruta.value = frutas[indice]
+            indice = (indice + 1) % frutas.length
+        }, 1500)
+
+        botaoAutomatico.textContent = 'Parar'
+    }
+}
+
+botaoAutomatico.addEventListener('click', automatico);
